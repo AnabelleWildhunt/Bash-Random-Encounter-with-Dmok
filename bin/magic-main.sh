a2b4a1725de807a90fe70e3089932c4ebf4e65a6
AnabelleWildhunt <mak.noemi5@gmail.com>
2026-07-28 19:52:20 +0200

    Added functionality to magic menu (currently only "go back" option)

--- bin/magic-main.sh
+++ bin/magic-main.sh
@@ -1,4 +1,29 @@
 #!/bin/bash
 # If player decides to cast magic
 
-echo "magic!"
+echo
+echo "#############"
+echo "#############"
+echo "#############"
+echo "#####  FB - Fireball"
+echo "#####  H - Heal"
+echo "#####  S - Swiftness"
+echo "#####  C - Charm"
+echo "#####  SH - Shield"
+echo "#####  D - Death"
+echo "#####  B - Go Back"
+echo "#############"
+echo "#############"
+echo "#############"
+
+read choice
+
+case $choice in
+	FB) echo;;
+	H) echo;;
+	S) echo;;
+	C) echo;;
+	SH) echo;;
+	D) echo;;
+	B) ./bin/menu.sh
+esac
